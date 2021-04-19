485eb9f7cf3a6e98e997a3e14e4f9ee411272c7b
carvalhojccs <jonne.cley@gmail.com>
2021-04-18 23:07:17 -0300

    API The Movie DB

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,9 +1,15 @@
-function Tempo(request, response) {
+async function Tempo(request, response) {
+    const apiSecret = process.env.API_KEY;
     const dynamicDate = new Date();
 
+    const movieResponse = await fetch(`https://api.themoviedb.org/3/movie/500?api_key=${API_KEY}&language=pt-BR`);
+    const movieResponseJson = await movieResponse.json();
+    const title = movieResponseJson.title;
+
     response.json({
-        date: dynamicDate.toUTCString()
-    })
+        date: dynamicDate.toUTCString(),
+        title: title
+    });
 }
 
 export default Tempo;
